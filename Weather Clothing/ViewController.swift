c8af98c7c1b063937e13e8046651f41ad591910c
Cesare de Cal <contact@cesare.io>
2018-11-01 16:18:37 +0100

    Created Xcode project

---
+++ Weather Clothing/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Weather Clothing
+//
+//  Created by Cesare de Cal on 11/1/18.
+//  Copyright © 2018 Cesare Gianfilippo Astianatte de Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
