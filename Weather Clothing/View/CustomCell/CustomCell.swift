7ef7d2486397c140a637a685eb3187607be0bd5c
Cesare de Cal <contact@cesare.io>
2018-11-01 22:50:46 +0100

    Creating new custom view cell to display details about the day

---
+++ Weather Clothing/View/CustomCell/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  Weather Clothing
+//
+//  Created by Cesare de Cal on 11/1/18.
+//  Copyright © 2018 Cesare Gianfilippo Astianatte de Cal. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    
+}
