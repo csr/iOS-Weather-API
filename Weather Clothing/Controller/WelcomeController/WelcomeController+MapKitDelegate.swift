3743b32ed37749395d7eb8eafc57606be5258116
Cesare de Cal <contact@cesare.io>
2018-11-01 20:55:37 +0100

    Implemented automatic location detection through MapKit

---
+++ Weather Clothing/Controller/WelcomeController/WelcomeController+MapKitDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  WelcomeController+MapKitDelegate.swift
+//  Weather Clothing
+//
+//  Created by Cesare de Cal on 11/1/18.
+//  Copyright © 2018 Cesare Gianfilippo Astianatte de Cal. All rights reserved.
+//
+
+import Foundation
