741397f7127323200f2bcd384bcc80fdebfba715
Cesare de Cal <contact@cesare.io>
2018-11-05 16:49:51 +0100

    Removed unnecessary Pods from project

--- Podfile
+++ Podfile
@@ -2,6 +2,5 @@ platform :ios, '10.0'
 
 target 'Weather Clothing' do
   use_frameworks!
-  pod 'AloeStackView'
   pod 'Eureka'
 end
--- Pods/Target Support Files/Pods-Weather Clothing/Pods-Weather Clothing-frameworks.sh
+++ Pods/Target Support Files/Pods-Weather Clothing/Pods-Weather Clothing-frameworks.sh
@@ -143,11 +143,9 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AloeStackView/AloeStackView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AloeStackView/AloeStackView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Eureka/Eureka.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
